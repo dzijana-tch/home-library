01e781613e226c179593227fe52ddce2890fa227
Dziyana Charniuk <dzijana.tch@yandex.by>
2024-05-03 00:46:33 +0500

    Упрощённая версия. Без реальной авторизации, без разграничения методов по юзерам, без проверки пароля, без кодировки пароля. Уникальный столбец в таблице книг - description.

--- src/main/java/by/charniuk/homelibrary/model/Book.java
+++ src/main/java/by/charniuk/homelibrary/model/Book.java
@@ -2,8 +2,6 @@ package by.charniuk.homelibrary.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
